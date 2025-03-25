a02033f3023ea67b8d2946fb48feca52bc71c811
Bryonych <bryony.c@gmail.com>
2025-03-25 10:27:55 +0300

    Remove file

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-    env: {
-      API_URL: 'https://x02ge7ylrf.execute-api.ap-southeast-2.amazonaws.com/dev/car',
-    }
-};
-
-export default nextConfig;
