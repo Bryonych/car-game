7290b2b6be81b820f46b28bb8cb969a437cb9bfe
Bryonych <bryony.c@gmail.com>
2025-05-12 14:50:12 +0300

    Add domain to frontend deployment stack

--- cdk/lib/frontend/frontend-stack.ts
+++ cdk/lib/frontend/frontend-stack.ts
@@ -1,11 +1,11 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
-// import * as acm from 'aws-cdk-lib/aws-certificatemanager';
+import * as acm from 'aws-cdk-lib/aws-certificatemanager';
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
 import * as cloudfront_origins from 'aws-cdk-lib/aws-cloudfront-origins';
-// import * as route53 from 'aws-cdk-lib/aws-route53';
-// import * as targets from 'aws-cdk-lib/aws-route53-targets';
+import * as route53 from 'aws-cdk-lib/aws-route53';
+import * as targets from 'aws-cdk-lib/aws-route53-targets';
 import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
 import * as path from 'path';
 import { fileURLToPath } from 'url';
@@ -20,33 +20,39 @@ export class FrontendStack extends cdk.NestedStack {
 
     const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
     const __dirname = path.dirname(__filename);
+    const domain = "revealthewheels.com";
+  
+    new cdk.CfnOutput(this, 'Site', {value: 'https://revealthewheels.com'});
 
-    // Add domain and uncomment when you have the domain
-    // new cdk.CfnOutput(this, 'Site', {value: 'https://test.com'});
-
+    // Create S3 bucket for built code deployment
     const gameBucket = new s3.Bucket(this, 'GameBucket', {
       bucketName: 'car-game-75',
       publicReadAccess: false,
       blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
-      // Need to change for prod
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      removalPolicy: cdk.RemovalPolicy.RETAIN,
       autoDeleteObjects: true,
     });
 
     new cdk.CfnOutput(this, 'Bucket', {value: gameBucket.bucketName });
 
-    // Uncomment when you have a domain
-    // const certificate = new acm.Certificate(this, 'SiteCertificate', {
-    //   domainName: 'https://test.com', // change
-    //   validation: acm.CertificateValidation.fromDns(); // add zone
-    // });
+    // fetch route53 zone
+    const zone = route53.HostedZone.fromLookup(this, "zone", {
+      domainName: domain,
+    })
 
-    // new cdk.CfnOutput(this, 'Certificate', {value: certificate.certificateArn});
+    // Create SSL cert for domain
+    const certificate = new acm.Certificate(this, 'SiteCertificate', {
+      domainName: domain,
+      validation: acm.CertificateValidation.fromDns(zone), 
+    });
 
+    new cdk.CfnOutput(this, 'Certificate', {value: certificate.certificateArn});
+
+    // Create Cloudfront distribution
     const distribution = new cloudfront.Distribution(this, 'SiteDistribution', {
-      // certificate: certificate, // uncomment when cert genenrated
+      certificate: certificate, 
       defaultRootObject: "index.html",
-      // domainNames: [// add domain name when created],
+      domainNames: [domain],
       minimumProtocolVersion: cloudfront.SecurityPolicyProtocol.TLS_V1_2_2021,
       errorResponses: [
         {
@@ -66,13 +72,14 @@ export class FrontendStack extends cdk.NestedStack {
 
     new cdk.CfnOutput(this, 'DistributionId', { value: distribution.distributionId });
 
-    // Uncomment when route53 set up
-    // new route53.ARecord(this, 'SiteAliasRecord', {
-    //   recordName: '', //add domain
-    //   target: route53.RecordTarget.fromAlias(new targets.CloudFrontTarget(distribution)),
-    //   zone // add zone
-    // });
+    // Add A record to hosted zone
+    new route53.ARecord(this, 'SiteAliasRecord', {
+      recordName: domain, 
+      target: route53.RecordTarget.fromAlias(new targets.CloudFrontTarget(distribution)),
+      zone: zone,
+    });
 
+    // Deploy build code to bucket
     new s3deploy.BucketDeployment(this, 'DeployWithInvalidation', {
       sources: [s3deploy.Source.asset(path.join(__dirname, '../../../out'))],
       destinationBucket: gameBucket,
