e53626684485e054be0eb5dad1141c99d2d5917f
Bryonych <bryony.c@gmail.com>
2025-05-12 15:10:41 +0300

    Add environment details

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -10,5 +10,8 @@ new CdkStack(app, 'CdkStack', {
 
   /* Uncomment the next line to specialize this stack for the AWS Account
    * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+  env: { 
+    account: process.env.CDK_DEFAULT_ACCOUNT, 
+    region: process.env.CDK_DEFAULT_REGION 
+  },
 });
