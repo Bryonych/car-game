cc359ef5e555638ebc05663dbbb3e78f0201c373
Bryonych <bryony.c@gmail.com>
2025-06-18 15:56:00 +0300

    Add view height fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import VHFix from "./components/VHFix.tsx";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,7 +25,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} bg-gray-100 min-h-screen` }>
+      <body className={`${geistSans.variable} ${geistMono.variable} bg-gray-100` } style={{ minHeight: 'calc(var(--vh, 1vh) * 100)' }}>
+        <VHFix />
         {children}
       </body>
     </html>
