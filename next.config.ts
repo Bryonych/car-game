def09aef945aee96a4de92a4d27242ddb396be79
Bryonych <bryony.c@gmail.com>
2026-06-07 16:14:41 +0300

    Change api call to go through cloudfront

--- next.config.ts
+++ next.config.ts
@@ -1,31 +1,10 @@
 import type { NextConfig } from "next";
-import { GetParameterCommand, SSMClient } from '@aws-sdk/client-ssm'
-
-
-const getApiUrl = async () => {
-  const client = new SSMClient({region: 'ap-southeast-2'});
-  const command = new GetParameterCommand({
-    Name: "/car-game/api-url",
-    WithDecryption: true,
-  });
-  try {
-    const response = await client.send(command);
-    return response.Parameter?.Value ?? '';
-  } catch (err) {
-    console.log("Error getting parameter", err);
-    return '';
-  }
-};
 
 const nextConfig: NextConfig = async () => {
-    const apiUrl = await getApiUrl();
     return {
       output: 'export',
       images: { unoptimized: true },
-      trailingSlash: true,
-      env: {
-        API_URL: apiUrl,
-      },
+      trailingSlash: false,
     }
     
 };
--- src/app/data/getData.tsx
+++ src/app/data/getData.tsx
@@ -4,10 +4,10 @@
  * @returns     An oject containing the image URL, the list of cars and the car data.
  */
 export async function getTodaysCar(date: string)  {
-    const api = process.env.API_URL;
+    const api = "/api/car";
     // console.log("API: " + api);
     try {
-        const res = await fetch(api! + "?date=" + date);
+        const res = await fetch(api + "?date=" + date);
         const resJson = await res.json();
         const carData = {
             "image": resJson.image, // Now a direct URL
